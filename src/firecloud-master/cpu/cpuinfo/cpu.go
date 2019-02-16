13bc50683860f4ef87dd2ba5103b98dc2401a7fa
wsyx123 <690943136@qq.com>
2019-02-17 02:07:36 +0800

    realize the function of slave pass struct and slice to master with grpc

--- src/firecloud-master/cpu/cpuinfo/cpu.go
+++ src/firecloud-master/cpu/cpuinfo/cpu.go
@@ -2,6 +2,7 @@ package cpuinfo
 
 import (
 	"fmt"
+	"reflect"
     "golang.org/x/net/context"
     "firecloud-master/cpu/cpuproto"
 )
@@ -12,7 +13,15 @@ type CpuInfo struct{}
 //实现CpuInfo接口
 func (c CpuInfo) Save(ctx context.Context, in *cpuproto.SaveRequest) (*cpuproto.SaveReply, error) {
     resp := new(cpuproto.SaveReply)
-    fmt.Println(in.Cpunum)
+		for _,val := range in.Pro{
+			tv := reflect.ValueOf(val).Elem()
+			typeOfT := tv.Type()
+			for i :=0; i < tv.NumField(); i++ {
+				fieldName := typeOfT.Field(i).Name
+				fieldValue := tv.FieldByName(fieldName)
+				fmt.Println(fieldName,fieldValue)
+			}
+		}
+		resp.Code = 200
     return resp, nil
 }
-
--- src/firecloud-slave/main.go
+++ src/firecloud-slave/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
     "fmt"
+    "reflect"
     "golang.org/x/net/context"
     "google.golang.org/grpc"
     "google.golang.org/grpc/grpclog"
-    "firecloud-slave/cpu/cpuproto" 
+    "firecloud-slave/cpu/cpuproto"
     "firecloud-slave/cpu/linux"
 )
 
@@ -23,22 +24,47 @@ func main() {
     }
 
     defer conn.Close()
-	
-	// 获取CPU信息
-	cpuinfos,err := linux.ReadCPUInfo("/proc/cpuinfo")
-	if err != nil {
-		grpclog.Fatal("stat read fail")
-	}
-	
-	cpunum := cpuinfos.NumCPU()
-	fmt.Println(cpunum)
-	
+
+    var processor_dict  = new(cpuproto.Processor)
+    var processor_list []*cpuproto.Processor
+
+  	// 获取CPU信息
+  	cpuinfos,err := linux.ReadCPUInfo("/proc/cpuinfo")
+  	if err != nil {
+  		grpclog.Fatal("stat read fail")
+  	}
+
+    // 遍历
+    // s := reflect.ValueOf(processor_dict).Elem()
+    // typeOfT := s.Type()
+    // for i := 0; i < s.NumField(); i++ {
+    //     // f := s.Field(i)
+    //     // fmt.Printf("%d %s %s = %v\n", i, typeOfT.Field(i).Name, f.Type(), f.Interface())
+    //     fmt.Printf("%s\n",typeOfT.Field(i).Name)
+    // }
+
+    // 遍历cpuinfos.Processors[i] 即 Processor
+    t1t := reflect.TypeOf(cpuinfos.Processors[0])
+    t1v := reflect.ValueOf(cpuinfos.Processors[0])
+    //processor_dict是一个指针，reflect.ValueOf(processor_dict)，reflect.ValueOf(*processor_dict) 传入的都只是拷贝
+    //Elem()表示传入指针引用，其它非指针需要加取地址符，表示传入引用 reflect.ValueOf(&cpuinfos.Processors[0])
+    t2v := reflect.ValueOf(processor_dict).Elem()
+    for j :=0; j < t1t.NumField(); j++ {
+      fieldName := t1t.Field(j).Name
+      fieldValue := t1v.FieldByName(fieldName)
+      t2v.FieldByName(fieldName).Set(fieldValue)
+    }
+    processor_dict.Id = 1
+    processor_list = append(processor_list,processor_dict)
+    fmt.Println(cpuinfos.Processors[0].Id)
+
     // 初始化客户端
     c := cpuproto.NewCpuInfoClient(conn)
 
     // 调用方法
     reqBody := new(cpuproto.SaveRequest)
-    reqBody.Cpunum = uint32(cpunum)
+    reqBody.Ip = "192.168.10.3"
+    reqBody.Pro = processor_list
     r, err := c.Save(context.Background(), reqBody)
     if err != nil {
         grpclog.Fatalln(err)
